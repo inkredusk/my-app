e1bb030ed35354359ecfc862ed400330e6c6e30d
Karan Balkar <karanbalkar@gmail.com>
2021-07-20 17:50:38 +0530

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -10,11 +10,7 @@ function App() {
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
-
-
-
-
-
+                <div> Hello         world</div>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
