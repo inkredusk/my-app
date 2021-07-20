3a61c7229529ebc907ec21bb9993a6ab732926bc
Karan Balkar <karanbalkar@gmail.com>
2021-07-20 18:56:19 +0530

    Modified App.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
         'plugin:react/recommended',
         'airbnb',
         'eslint:recommended',
-        'plugin:react/recommended',
         'plugin:react-hooks/recommended',
         'plugin:prettier/recommended',
         'plugin:jsx-a11y/strict',
