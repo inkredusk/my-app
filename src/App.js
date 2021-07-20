c62b8f8ec45a370ee6161afaad5c7a0dc40a6a54
Karan Balkar <karanbalkar@gmail.com>
2021-07-20 19:49:39 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-let test;
+var test;
 function App() {
     return (
         <div className="App">
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-var tets;
+let tets;
 
 ReactDOM.render(
     <React.StrictMode>
