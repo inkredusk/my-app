b7d4b74821b502e1f2557aaf5238ddcd69dac478
Karan Balkar <karanbalkar@gmail.com>
2021-07-20 17:49:38 +0530

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
     return (
@@ -9,14 +10,6 @@ function App() {
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
-
-
-
-
-
-
-
-
 
                 <a
                     className="App-link"
